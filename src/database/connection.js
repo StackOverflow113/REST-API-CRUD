32a2bbd5d23e0151af797fbbe43a71638a312db5
Carlos Moreno <carloseduardomorenocamacho@gmail.com>
2022-01-19 21:46:31 -0700

    First Commit

---
+++ src/database/connection.js
@@ -0,0 +1,20 @@
+import sql from 'mssql'; //SE IMPORTA EL MODULO 
+
+const dbSettings = { //CONFIGURACION DE LA BASE DE DATOS COMO SERVIDOR, USAURIO Y CONTRASENIA
+    user: 'TEST',
+    password: 'naruto200',
+    server: 'localhost', // SERVER SE REFIERE A LA IP DONDE ESTA EL SERVIDOR POR DEFAULT ES 127.0.0.1 OR LOCALHOST
+    database: 'CatalogosSPC',
+    options: {
+        encrypt: true, // for azure
+        trustServerCertificate: true // change to true for local dev / self-signed certs
+    }
+};
+
+async function getConnection() { // SE CREA UNA FUNCION PARA PROBAR CONECTAR A TRAVEZ DE LOS SIGUIENTES PARAMETROS A NUESTRA BASE DE DATOS. 
+    const pool = await sql.connect(dbSettings);
+    const result = await pool.request().query("SELECT CaUSUA_tUsuario,CaUSUA_tPassword FROM CatalogosSPC.dbo.CaUsuarios");
+    console.log(result);
+}
+//AUN NO SE EJECUTA POR QUE NO HAY NINGUNA PARTE QUE LO MANDE A LLAMAR ENTONCES LO MANDAREMOS A LLAMAR DESDE EL INDEX.JS
+getConnection();
